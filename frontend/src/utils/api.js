f8dbbb624588e2e711869e332f431c9c280d3849
Joel Lord <joelphy@gmail.com>
2022-11-27 06:23:43 -0500

    Start

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,41 +1,15 @@
 import config from "../config";
 
 const getRecipes = async () => {
-  const url = `${config.BASE_URL}/recipes`;
-  let data = await fetch(url).then(resp => resp.json());
-  return data;
+  // TODO
 }
 
-// const getRecipes = async () => {
-//   const url = `${config.BASE_URL}/findMany`;
-//   const headers = {
-//     "Content-Type": "application/json",
-//     "Access-Control-Request-Headers": "*",
-//     "api-key": config.DATA_API_KEY
-//   };
-//   const dataRaw = {
-//     collection: "recipes",
-//     database: "recipeBook",
-//     dataSource :"Cluster0"
-//   }
-//   const data = await fetch(url, {
-//     method: "POST",
-//     headers,
-//     body: JSON.stringify(dataRaw)
-//   }).then(resp => resp.json());
-//   return data;
-// }
-
 const getChefs = async () => {
-  const url = `${config.BASE_URL}/chefs`;
-  let data = await fetch(url).then(resp => resp.json());
-  return data;
+  // TODO 
 }
 
 const searchRecipes = async (searchTerm) => {
-  const url = `${config.BASE_URL}/search?search=${searchTerm}`
-  let data = await fetch(url).then(resp => resp.json());
-  return data;
+  // TODO
 }
 
 export {
